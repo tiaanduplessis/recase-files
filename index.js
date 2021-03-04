873d6c42ba4453fdd0e8b22b440fe9a89673c8c9
= <tiaanduplessis@hotmail.com>
2021-03-05 00:01:17 +0200

    chore: add and run lint

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 #!/usr/bin/env node
-const path = require("path")
-const fs = require("fs/promises")
+const path = require('path')
+const fs = require('fs/promises')
 
-const globby = require('globby');
-const args = require('get-them-args')();
+const globby = require('globby')
+const args = require('get-them-args')()
 const casey = require('casey-js')
 
-const pkg = require('./package.json');
+const pkg = require('./package.json')
 
 const transformers = {
-    kebab: casey.toKebabCase,
-    snake: casey.toSnakeCase,
-    train: casey.toTrainCase,
-    pascal: casey.toPascalCase,
-    camel: casey.toCamelCase,
-    lower: casey.toLowerCase,
-    upper: casey.toUpperCase
+  kebab: casey.toKebabCase,
+  snake: casey.toSnakeCase,
+  train: casey.toTrainCase,
+  pascal: casey.toPascalCase,
+  camel: casey.toCamelCase,
+  lower: casey.toLowerCase,
+  upper: casey.toUpperCase
 };
 
 (async () => {
-    if (args.version) {
-        console.log(pkg.version)
-        return
-    }
+  if (args.version) {
+    console.log(pkg.version)
+    return
+  }
 
-    if (!args.transform && !args.t) {
-        throw new Error(`Please select a transform to apply (${Object.keys(transformers).join(', ')})`)
-    }
+  if (!args.transform && !args.t) {
+    throw new Error(`Please select a transform to apply (${Object.keys(transformers).join(', ')})`)
+  }
 
-	const paths = await globby(args.unknown);
-    const transform = transformers[args.transform || args.t]
+  const paths = await globby(args.unknown)
+  const transform = transformers[args.transform || args.t]
 
-    await Promise.all(paths.map(async current => {
-        const oldName = path.parse(current).name
-        const transformedName = transform(oldName)
-        return fs.rename(current, current.replace(oldName, transformedName))
-    }))
-})();
+  await Promise.all(paths.map(async current => {
+    const oldName = path.parse(current).name
+    const transformedName = transform(oldName)
+    return fs.rename(current, current.replace(oldName, transformedName))
+  }))
+})()
